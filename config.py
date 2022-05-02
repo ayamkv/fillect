6402a988d00bc09328ff08e6d7ed6c9147299821
ayamkv <realayamkw@gmail.com>
2022-05-02 22:26:41 +0700

    added randomize

--- config.py
+++ config.py
@@ -5,9 +5,14 @@ apikey_secret = '#'
 token = '#'
 token_secret = '#'
 
+# CUSTOM
+randomize = True
+
 # LOOP 
 loop = True
 hours = 2   
 # loops every ... seconds 
 # ignore if loop is False
 sleeptime = hours * 60 * 60
+
+# 
